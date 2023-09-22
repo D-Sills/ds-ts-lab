50d68b1855a75533ca8a50ba26c245ee4ea7d65c
Darren Sills <darrsills@gmail.com>
2023-09-22 14:00:47 +0100

    Added type-safety to colleague data

---
+++ src/01-basics.ts
@@ -0,0 +1,63 @@
+interface Friend {
+  name: string;
+  phone: string;
+  age: number;
+}
+
+const friend1: Friend = {
+  name: "I",
+  phone: "087-12345",
+  age: 25,
+};
+
+const friend2: Friend = {
+  name: "Have",
+  phone: "086--12345",
+  age: 31,
+};
+
+const friends: Friend[] = [friend1, friend2];
+console.log(friends[1]);
+
+//   -------------------
+interface Colleague {
+  name: string;
+  department: string;
+  contact: {
+    email: string;
+    extension: number;
+  };
+}
+
+const colleague1: Colleague = {
+  name: "No",
+  department: "Engineering",
+  contact: {
+    email: "rgraham@company.com",
+    extension: 121,
+  },
+};
+
+const colleague2: Colleague = {
+  name: "Friends",
+  department: "Finance",
+  contact: {
+    email: "pburke@company.com",
+    extension: 132,
+  },
+};
+
+const colleague3: Colleague = {
+  name: ":(",
+  department: "HR",
+  contact: {
+    email: "dos@company.com",
+    extension: 125,
+  },
+};
+const colleagues = {
+  current: [colleague1, colleague2, colleague3],
+  former: [],
+};
+
+console.log(colleagues.current[0]);
